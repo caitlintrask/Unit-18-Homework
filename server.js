e378d46a23fd744bf7bb614a896ae70a10632dc9
Caitlin Trask <caitlintrask@gmail.com>
2020-04-16 10:40:43 -0600

    added new mongoose connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://caitlintrask:password1@ds237707.mlab.com:37707/heroku_n6vj5z7x", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
